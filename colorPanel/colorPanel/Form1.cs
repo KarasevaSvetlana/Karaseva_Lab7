0d292b495a269018224af55bc5455e78b0fa8f51
Светлана Краснокутская <krasnokutskaya.sa@edu.spbstu.ru>
2020-05-15 13:24:03 +0300

    Добавьте файлы проекта.

---
+++ colorPanel/colorPanel/Form1.cs
@@ -0,0 +1,82 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace colorPanel
+{
+    public partial class Form1 : Form
+    {
+        static int valueR = 125;
+        static int valueG = 125;
+        static int valueB = 125;
+        string myColor = "#000";
+        public Form1()
+        {
+            InitializeComponent();
+        }
+
+        private void tableLayoutPanel1_Paint(object sender, PaintEventArgs e)
+        {
+           
+        }
+
+        private void pictureBox1_MouseMove(object sender, MouseEventArgs e)
+        {
+            Color color = pictureBox1.BackColor;
+            
+            byte g = color.G;
+            byte b = color.B;
+            byte r = color.R;
+            myColor = $"#{r:X2}{g:X2}{b:X2}";
+            toolTip1.SetToolTip(pictureBox1,$"Ваш цвет: "+myColor); 
+            
+        }
+ 
+        private void trackBar2_Scroll(object sender, EventArgs e)
+        {
+          
+            valueR = trackBar2.Value;
+            pictureBox1.BackColor = Color.FromArgb(valueR, valueG, valueB);
+            Color color = pictureBox1.BackColor;
+
+            byte g = color.G;
+            byte b = color.B;
+            byte r = color.R;
+            myColor = $"#{r:X2}{g:X2}{b:X2}";
+            Clipboard.SetDataObject(myColor);
+        }
+
+        private void trackBar3_Scroll(object sender, EventArgs e)
+        {
+
+            valueG = trackBar3.Value;
+            pictureBox1.BackColor = Color.FromArgb(valueR, valueG, valueB);
+            Color color = pictureBox1.BackColor;
+
+            byte g = color.G;
+            byte b = color.B;
+            byte r = color.R;
+            myColor = $"#{r:X2}{g:X2}{b:X2}";
+            Clipboard.SetDataObject(myColor);
+        }
+
+        private void trackBar4_Scroll(object sender, EventArgs e)
+        {
+            valueB = trackBar4.Value;
+            pictureBox1.BackColor = Color.FromArgb(valueR, valueG, valueB);
+            Color color = pictureBox1.BackColor;
+
+            byte g = color.G;
+            byte b = color.B;
+            byte r = color.R;
+            myColor = $"#{r:X2}{g:X2}{b:X2}";
+            Clipboard.SetDataObject(myColor);
+        }
+    }
+}
